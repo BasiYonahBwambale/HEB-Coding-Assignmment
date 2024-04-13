1c46eb39774ad26d55e7bdbe41ccfb8879bd2588
Basi Yonah Bwambale <basiyonah@gmail.com>
2024-04-13 10:55:27 -0400

    Initial Commit

---
+++ src/main/java/com/heb/hebcustomerservice/HebcustomerserviceApplication.java
@@ -0,0 +1,13 @@
+package com.heb.hebcustomerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HebcustomerserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HebcustomerserviceApplication.class, args);
+	}
+
+}
