23dc817fb13df7e765e53bef9e84a6cc4aaa4e64
Basi Yonah Bwambale <basiyonah@gmail.com>
2024-04-13 15:38:12 -0400

    add getallcustomers endpoint

--- src/main/java/com/heb/hebcustomerservice/controller/CustomerControllerImpl.java
+++ src/main/java/com/heb/hebcustomerservice/controller/CustomerControllerImpl.java
@@ -1,14 +1,30 @@
 package com.heb.hebcustomerservice.controller;
 
 import com.heb.hebcustomerservice.entity.Customer;
+import com.heb.hebcustomerservice.service.CustomerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.UUID;
 
+@RestController()
+@RequestMapping(path = "/customers")
 public class CustomerControllerImpl implements CustomerController {
+    private final CustomerServiceImpl customerService;
+    @Autowired
+    public CustomerControllerImpl(CustomerServiceImpl customerService) {
+        this.customerService = customerService;
+    }
     @Override
-    public ResponseEntity findAllCustomers() {
-        return null;
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<Customer>> findAllCustomers() {
+     return ResponseEntity.ok(customerService.findAllCustomers());
     }
 
     @Override
