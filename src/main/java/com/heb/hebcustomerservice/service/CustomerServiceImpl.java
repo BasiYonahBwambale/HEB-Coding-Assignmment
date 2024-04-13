4357eec14343fa647008412e8c5821fb83eceeff
Basi Yonah Bwambale <basiyonah@gmail.com>
2024-04-13 14:35:18 -0400

    add findAllCustomers implementation to pass test

--- src/main/java/com/heb/hebcustomerservice/service/CustomerServiceImpl.java
+++ src/main/java/com/heb/hebcustomerservice/service/CustomerServiceImpl.java
@@ -1,14 +1,21 @@
 package com.heb.hebcustomerservice.service;
 
 import com.heb.hebcustomerservice.entity.Customer;
+import com.heb.hebcustomerservice.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-
+@Service
 public class CustomerServiceImpl implements CustomerService{
+    private CustomerRepository customerRepository;
+private CustomerServiceImpl(CustomerRepository customerRepository){
+    this.customerRepository = customerRepository;
+}
     @Override
     public List<Customer> findAllCustomers() {
-        return null;
+        return customerRepository.findAll();
     }
 
     @Override
