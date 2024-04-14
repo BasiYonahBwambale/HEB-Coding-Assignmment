1ea349fc85c19b0fb1e8d0f4ec1c469d6f1f5b1f
Basi Yonah Bwambale <basiyonah@gmail.com>
2024-04-14 11:12:08 -0400

    add query for findCustomerByCity

--- src/main/java/com/heb/hebcustomerservice/repository/CustomerRepository.java
+++ src/main/java/com/heb/hebcustomerservice/repository/CustomerRepository.java
@@ -2,10 +2,12 @@ package com.heb.hebcustomerservice.repository;
 
 import com.heb.hebcustomerservice.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface CustomerRepository extends JpaRepository<Customer, UUID> {
-    List<Customer> findCustomerByCity(String city);
+    @Query("SELECT c FROM Customer c WHERE c.city = :customerCity")
+    List<Customer> findCustomerByCity(String customerCity);
 }
